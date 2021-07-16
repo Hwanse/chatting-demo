5906e0fa98fedeeeb34b0203cfbc2a31dca805d6
hwanse <khsb2012@gmail.com>
2021-07-16 18:31:18 +0900

    feat: spring 프로젝트 생성

---
+++ server/src/test/java/me/hwanse/chatserver/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwanse.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
