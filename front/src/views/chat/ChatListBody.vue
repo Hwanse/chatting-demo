3bb951740b4182eb71fbc7840621b71c3a96ccfd
hwanse <khsb2012@gmail.com>
2021-07-18 20:28:55 +0900

    feat: 채팅 목록 view 정리, 채팅방 개설 버튼 추가, 채팅방 view route 추가

--- front/src/views/chat/ChatListBody.vue
+++ front/src/views/chat/ChatListBody.vue
@@ -3,45 +3,70 @@
         <v-card>
             <v-virtual-scroll
                 :items="chatList"
-                item-height="70"
+                item-height="55"
                 height="650"
             >
                 <template v-slot="{ item }">
                     <chat-room-component :key="item.id" :chat="item"></chat-room-component>
                 </template>
             </v-virtual-scroll>
+            
+            <!-- <div class="fixed-button">
+                <v-btn 
+                    fab
+                    dark
+                    color="pink"
+                    @click="changeShowForm"
+                >
+                    <v-icon>mdi-plus</v-icon>
+                </v-btn>
+            </div> -->
+
         </v-card>
     </div>
 </template>
 
 <script>
 import ChatRoomComponent from "./ChatRoomComponent.vue"
+// import ChatRoomCreateForm from "./ChatRoomCreateForm.vue"
 import axios from "axios"
 
 export default {
     data() {
         return {
-            chatList: []
+            chatList: [],
+            showForm: false
         }
     },
     mounted() {
         this.getChatList()
     },
     components: {
-        'chat-room-component': ChatRoomComponent
+        'chat-room-component': ChatRoomComponent,
+        // 'chat-room-create-form': ChatRoomCreateForm
     },
     methods: {
         getChatList() {
             axios.get(`${location.protocol}//${location.host}/api/chat-room`)
                 .then(response => {
-                    console.log(response.data)
                     this.chatList = response.data
                 })
                 .catch(error => {
                     console.log(error)
                 })
+        },
+        changeShowForm() {
+            this.showForm = !this.showForm
         }
     }
 
 }
 </script>
+
+<style>
+.fixed-button {
+    position: absolute;
+    top: 80%;
+    right: 10%;
+}
+</style>
---
+++ front/src/views/chat/chatRoom/ChatRoomView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <chat-room-head></chat-room-head>
+        <chat-room-body></chat-room-body>
+        <h2>채팅방</h2>
+    </div>
+</template>
+
+<script>
+import ChatRoomHead from "./ChatRoomHead.vue"
+import ChatRoomBody from "./ChatRoomBody.vue"
+
+export default {
+    components: {
+        'chat-room-head': ChatRoomHead,
+        'chat-room-body': ChatRoomBody
+    }
+}
+</script>
