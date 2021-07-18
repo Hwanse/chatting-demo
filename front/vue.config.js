2d2fd1014b5cec42d1961612149f84b140dacfcc
hwanse <khsb2012@gmail.com>
2021-07-18 15:53:28 +0900

    feat: front project setting

---
+++ front/vue.config.js
@@ -0,0 +1,16 @@
+const path = require("path")
+
+module.exports = {
+//   outputDir: path.resolve(__dirname, `../server/src/main/resources/static`),
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8081",
+        changeOrigin: true, // cors 문제를 막기위한 옵션
+        // ws: true,   // proxy websocket
+      },
+    },
+  },
+
+  transpileDependencies: ["vuetify"],
+};
