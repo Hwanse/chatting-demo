9e5ec6b21ee88c4fbc3e5a2a3657baf5054f38ff
hwanse <khsb2012@gmail.com>
2021-07-19 18:47:15 +0900

    feat: 채팅 메세지 전송 후 auto scroll 조작 로직 추가

--- front/src/views/chat/chatRoom/ChatRoomView.vue
+++ front/src/views/chat/chatRoom/ChatRoomView.vue
@@ -1,15 +1,36 @@
 <template>
     <div>
-        <chat-room-head></chat-room-head>
-        <chat-room-body></chat-room-body>
+        <chat-room-head :info="room"></chat-room-head>
+        <chat-room-body :info="room"></chat-room-body>
     </div>
 </template>
 
 <script>
 import ChatRoomHead from "./ChatRoomHead.vue"
 import ChatRoomBody from "./ChatRoomBody.vue"
+import axios from "axios";
 
 export default {
+    data() {
+        return {
+            room: {}
+        }
+    },
+    created() {
+        this.findRoom()
+    },
+    methods: {
+        findRoom() {
+            let roomId = this.$route.params.id
+            axios.get(`${location.protocol}//${location.host}/api/chat-room/${roomId}`)
+                .then(response => {
+                    this.room = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
+    },
     components: {
         'chat-room-head': ChatRoomHead,
         'chat-room-body': ChatRoomBody
