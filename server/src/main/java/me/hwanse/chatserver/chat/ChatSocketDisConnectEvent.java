6da8a26c40f713c5d85d175cb4b9f6b9946a0b91
hwanse <khsb2012@gmail.com>
2021-07-30 01:11:53 +0900

    feat: session disconnect event 처리 추가
    - 강제로 브라우저를 나간 유저에 대한 event 처리 시 예외 상황이 빈번하므로 대책 방안이 필요

---
+++ server/src/main/java/me/hwanse/chatserver/chat/ChatSocketDisConnectEvent.java
@@ -0,0 +1,23 @@
+package me.hwanse.chatserver.chat;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.hwanse.chatserver.chatroom.service.ChatVisitorService;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ChatSocketDisConnectEvent implements ApplicationListener<SessionDisconnectEvent> {
+
+    private final ChatVisitorService chatVisitorService;
+
+    @Override
+    public void onApplicationEvent(SessionDisconnectEvent event) {
+        String sessionId = event.getSessionId();
+        chatVisitorService.leaveChatVisitor(sessionId);
+    }
+
+}
