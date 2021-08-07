db8a7a2d495653caff93b0c816d445f3fd178a11
hwanse <khsb2012@gmail.com>
2021-08-07 23:45:24 +0900

    feat: jwt 관련 라이브러리 의존성 및 환경 설정 추가

--- server/src/main/java/me/hwanse/chatserver/chat/ChatController.java
+++ server/src/main/java/me/hwanse/chatserver/chat/ChatController.java
@@ -3,6 +3,7 @@ package me.hwanse.chatserver.chat;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.hwanse.chatserver.chat.text.ChatMessage;
+import me.hwanse.chatserver.chat.text.MessageType;
 import me.hwanse.chatserver.chat.text.MonitoringMessage;
 import me.hwanse.chatserver.chat.voice.IceMessage;
 import me.hwanse.chatserver.chat.voice.LeaveMessage;
@@ -22,6 +23,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import javax.validation.Valid;
 
+import static me.hwanse.chatserver.chat.text.MessageType.*;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -34,40 +37,41 @@ public class ChatController {
     // =========== 채팅방 공통 ==============
     @MessageMapping("/join")
     public void joinInChat(@Valid ChatMessage chatMessage) {
-        messagingTemplate.convertAndSend(getDestination(chatMessage.getRoomId()), chatMessage);
+        messagingTemplate.convertAndSend(getDestination(chatMessage.getRoomId(), JOIN), chatMessage);
     }
 
     @MessageMapping("/monitoring")
     public void monitoringUserCountInChatRoom(SimpMessageHeaderAccessor headerAccessor, @Valid MonitoringMessage message) {
         message.setUserCount(chatVisitorService.findVisitorsInTheChatRoom(message.getRoomId()).size());
-        messagingTemplate.convertAndSendToUser(headerAccessor.getSessionId(), getDestination(message.getRoomId()) + "/monitoring", message);
+        messagingTemplate.convertAndSendToUser(headerAccessor.getSessionId(),
+                getDestination(message.getRoomId(), MONITORING), message);
     }
 
     // =========== 텍스트 채팅 관련 ==============
     @MessageMapping("/text/message")
     public void sendMessage(@Valid ChatMessage chatMessage) {
-        messagingTemplate.convertAndSend(getDestination(chatMessage.getRoomId()), chatMessage);
+        messagingTemplate.convertAndSend(getDestination(chatMessage.getRoomId(), TALK), chatMessage);
     }
 
     // =========== 음성 채팅 관련 ==============
     @MessageMapping("/voice/offer")
     public void sendVoiceToPeers(@Valid SdpMessage message) {
-        messagingTemplate.convertAndSendToUser(message.getToId(), getDestination(message.getRoomId()) + "/voice", message);
+        messagingTemplate.convertAndSendToUser(message.getToId(), getDestination(message.getRoomId(), VOICE), message);
     }
 
     @MessageMapping("/voice/candidate")
     public void sendVoiceToPeers(@Valid IceMessage message) {
-        messagingTemplate.convertAndSendToUser(message.getToId(), getDestination(message.getRoomId()) +"/voice", message);
+        messagingTemplate.convertAndSendToUser(message.getToId(), getDestination(message.getRoomId(), VOICE), message);
     }
 
     @MessageMapping("/voice/leave")
     public void leaveChatNotification(@Valid LeaveMessage leaveMessage) {
         chatVisitorService.leaveChatVisitorInChatRoom(leaveMessage.getRoomId(), leaveMessage.getSessionId());
-        messagingTemplate.convertAndSend(getDestination(leaveMessage.getRoomId()) + "/leave", leaveMessage.getSessionId());
+        messagingTemplate.convertAndSend(getDestination(leaveMessage.getRoomId(), LEAVE), leaveMessage.getSessionId());
     }
 
-    private String getDestination(Long roomId) {
-        return String.format("/sub/chat-room/%d", roomId);
+    private String getDestination(Long roomId, MessageType messageType) {
+        return String.format("/sub/chat-room/%d%s", roomId, messageType.getDestination());
     }
 
     @MessageExceptionHandler({MethodArgumentNotValidException.class, IllegalArgumentException.class,
--- server/src/main/java/me/hwanse/chatserver/chat/text/MessageType.java
+++ server/src/main/java/me/hwanse/chatserver/chat/text/MessageType.java
@@ -1,5 +1,16 @@
 package me.hwanse.chatserver.chat.text;
 
 public enum MessageType {
-    JOIN, TALK, MONITOR
+    JOIN(""), TALK(""), MONITORING("/monitoring"),
+    VOICE("/voice"), LEAVE("/leave");
+
+    private String destination;
+
+    MessageType(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
 }
