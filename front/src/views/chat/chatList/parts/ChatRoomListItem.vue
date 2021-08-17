62dd3e23c9ff3323729d08e977879e391882fed1
hwanse <khsb2012@gmail.com>
2021-08-17 12:33:34 +0900

    feat: 제한 인원 만큼 가득찬 채팅방 입장 제한 기능 추가

--- front/src/views/chat/chatList/parts/ChatRoomListItem.vue
+++ front/src/views/chat/chatList/parts/ChatRoomListItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <v-item-group >
-            <v-list-item :to="linkTo">
+            <v-list-item @click="enterChatRoom">
                 <v-list-item-content>
                     <v-list-item-title>
                         {{chatRoom.title}}
@@ -18,12 +18,28 @@
 </template>
 
 <script>
+import chatApi from "@/api/chat/chatAPI.js"
+
 export default {
     props: ['chat'],
     data() {
         return {
             chatRoom: this.chat,
             linkTo: `/chat/${this.chat.id}`
+        }
+    }, 
+    methods: {
+        async enterChatRoom() {
+            const room = await chatApi.getChatRoom(this.chat.id)
+            if (room) {
+                (room.limitUserCount - room.userCount > 0) ? this.goToChatRoom() : this.alertMessage()
+            }
+        },
+        goToChatRoom() {
+            this.$router.push({name: "ChatRoomView", params: {id: this.chat.id}})
+        },
+        alertMessage() {
+            alert("현재 채팅방 인원이 가득 찼습니다.")
         }
     }
 }
