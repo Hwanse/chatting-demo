820b45694476a354c28d3f7bb692b6d19c526c5f
hwanse <khsb2012@gmail.com>
2021-08-19 00:56:19 +0900

    feat: 프로젝트 빌드 시 index asciidoc 이 생성이 안되는 현상 수정

--- server/src/test/java/me/hwanse/chatserver/user/controller/AuthenticateControllerTest.java
+++ server/src/test/java/me/hwanse/chatserver/user/controller/AuthenticateControllerTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
