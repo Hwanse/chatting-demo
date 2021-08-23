ba88351e38b1c5c97669ec268895f183b3dcdb89
hwanse <khsb2012@gmail.com>
2021-08-23 17:02:30 +0900

    feat: local, dev 환경 프로파일 분리, 로깅 설정, 코드 정리
    - 서버의 local과 dev 환경에 따른 설정을 분리한다
    - 로깅 레벨을 local, dev 환경 별로 분리하여 설정
      - 로그 파일 생성은 이후 개발서버용 클라우드에서 테스트하면서 추가 필요
    - 안쓰이는 코드 및 클래스 파일 정리

---
+++ server/src/main/java/me/hwanse/chatserver/api/GeneralExceptionHandler.java
@@ -0,0 +1,69 @@
+package me.hwanse.chatserver.api;
+
+import lombok.extern.slf4j.Slf4j;
+import me.hwanse.chatserver.exception.DuplicateException;
+import me.hwanse.chatserver.exception.JwtClaimsVerifyError;
+import me.hwanse.chatserver.exception.NotFoundException;
+import me.hwanse.chatserver.exception.NotHaveManagerPrivilege;
+import me.hwanse.chatserver.exception.ServiceRuntimeException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GeneralExceptionHandler {
+
+    private ResponseEntity<ApiResult<?>> makeErrorResponse(HttpStatus status, Throwable throwable) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        return new ResponseEntity<>(ApiResult.ERROR(throwable, status), headers, status);
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
+    public ResponseEntity handleClientBadRequest(Exception e) {
+        log.debug("Client bad request exception occurred : {}", e.getMessage(), e);
+        return makeErrorResponse(HttpStatus.BAD_REQUEST, e);
+    }
+
+    @ExceptionHandler(HttpMediaTypeException.class)
+    public ResponseEntity handleMediaTypeException(Exception e) {
+        return makeErrorResponse(HttpStatus.UNSUPPORTED_MEDIA_TYPE, e);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity handleRequestMethodNotSupportedException(Exception e) {
+        return makeErrorResponse(HttpStatus.METHOD_NOT_ALLOWED, e);
+    }
+
+    @ExceptionHandler(NotHaveManagerPrivilege.class)
+    public ResponseEntity handleNotHaveManagerPrivilegeException(NotHaveManagerPrivilege e) {
+        return makeErrorResponse(HttpStatus.FORBIDDEN, e);
+    }
+
+    @ExceptionHandler(ServiceRuntimeException.class)
+    public ResponseEntity handleServiceException(ServiceRuntimeException e) {
+        if (e instanceof NotFoundException) {
+            return makeErrorResponse(HttpStatus.BAD_REQUEST, e);
+        } else if (e instanceof DuplicateException) {
+            return makeErrorResponse(HttpStatus.CONFLICT, e);
+        } else if (e instanceof JwtClaimsVerifyError) {
+            return makeErrorResponse(HttpStatus.UNAUTHORIZED, e);
+        }
+
+        log.error("Unexpected service Exception occurred : {}", e.getMessage(), e);
+        return makeErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e);
+    }
+
+    @ExceptionHandler({Exception.class, RuntimeException.class})
+    public ResponseEntity handleUnexpectedException(Exception e) {
+        log.error("Unexpected exception occurred : {}", e.getMessage(), e);
+        return makeErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e);
+    }
+
+}
