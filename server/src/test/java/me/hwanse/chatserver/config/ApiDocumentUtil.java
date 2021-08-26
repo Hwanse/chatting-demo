a2b888fba9be77254ce72ca957fb36ae928eed40
hwanse <khsb2012@gmail.com>
2021-08-26 17:55:05 +0900

    test: Rest Docs 문서화 테스트 코드 공통 response 필드 Util 클래스로 분리

--- server/src/test/java/me/hwanse/chatserver/config/ApiDocumentUtil.java
+++ server/src/test/java/me/hwanse/chatserver/config/ApiDocumentUtil.java
@@ -1,16 +1,51 @@
 package me.hwanse.chatserver.config;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.core.env.Environment;
 import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 
 public class ApiDocumentUtil {
 
     public static OperationRequestPreprocessor getDocumentRequest() {
         return preprocessRequest(
-            modifyUris().removePort()
+            modifyUris()
+            .host("34.64.244.142")
+            .port(8081)
         );
+    }
+
+    public static FieldDescriptor[] commonResponseFields() {
+        return new FieldDescriptor[] {
+            fieldWithPath("success").description("api 요청결과 성공 여부"),
+            fieldWithPath("data").description("api 응답 데이터 본문"),
+            fieldWithPath("error").optional().type(
+                JsonFieldType.OBJECT).description("api 에러 내용(에러가 없을경우 null)")
+        };
+    }
+
+    public static List<FieldDescriptor> makeResponseFields(FieldDescriptor... responseFields) {
+        return Stream.concat(
+            Stream.of(commonResponseFields())
+            , Stream.of(responseFields)
+        ).collect(Collectors.toList());
+    }
+
+    public static List<FieldDescriptor> makeResponseFields(List<FieldDescriptor> responseFields) {
+        return Stream.concat(
+            Stream.of(commonResponseFields())
+            , responseFields.stream()
+        ).collect(Collectors.toList());
     }
 
 }
