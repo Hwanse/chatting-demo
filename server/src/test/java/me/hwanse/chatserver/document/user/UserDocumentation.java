55a2098c5fafd3adae6bd963e269a6e6c9620a52
hwanse <khsb2012@gmail.com>
2021-08-29 22:16:47 +0900

    style: rest docs 코드 스타일 정리

--- server/src/test/java/me/hwanse/chatserver/document/user/UserDocumentation.java
+++ server/src/test/java/me/hwanse/chatserver/document/user/UserDocumentation.java
@@ -14,45 +14,47 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 public class UserDocumentation {
 
     public static RestDocumentationResultHandler signUpApiDocument() {
-        return document("user/signup", ApiDocumentUtil.getDocumentRequest(),
+        return document("user/signup",
+                        ApiDocumentUtil.getDocumentRequest(),
                         requestHeaders(
-                        headerWithName(HttpHeaders.CONTENT_TYPE).description(MediaType.APPLICATION_JSON)
-                    ),
+                            headerWithName(HttpHeaders.CONTENT_TYPE).description(MediaType.APPLICATION_JSON)
+                        ),
                         requestFields(
-                        fieldWithPath("userId").description("회원가입 - 유저ID"),
-                        fieldWithPath("password").description("회원가입 - 비밀번호")
-                    ),
+                            fieldWithPath("userId").description("회원가입 - 유저ID"),
+                            fieldWithPath("password").description("회원가입 - 비밀번호")
+                        ),
                         responseFields(
                             ApiDocumentUtil.makeResponseFields(
-                            fieldWithPath("data.id").description("회원가입된 유저의 key 값"),
-                            fieldWithPath("data.userId").description("회원가입된 유저의 ID"),
-                            fieldWithPath("data.createdAt").description("회원가입 날짜"),
-                            fieldWithPath("data.updatedAt").description("회원수정 날짜(default - 최초에는 회원 가입 날짜)"),
-                            fieldWithPath("data.use").description("회원의 사용 여부"),
-                            fieldWithPath("data.links").optional().type(JsonFieldType.ARRAY).description("서버 인증 토큰 발급 링크, profile 링크의 모음"),
-                            fieldWithPath("data.links[].*").ignored()
+                                fieldWithPath("data.id").description("회원가입된 유저의 key 값"),
+                                fieldWithPath("data.userId").description("회원가입된 유저의 ID"),
+                                fieldWithPath("data.createdAt").description("회원가입 날짜"),
+                                fieldWithPath("data.updatedAt").description("회원수정 날짜(default - 최초에는 회원 가입 날짜)"),
+                                fieldWithPath("data.use").description("회원의 사용 여부"),
+                                fieldWithPath("data.links").optional().type(JsonFieldType.ARRAY).description("서버 인증 토큰 발급 링크, profile 링크의 모음"),
+                                fieldWithPath("data.links[].*").ignored()
+                            )
                         )
-                    )
-                );
+        );
     }
 
     public static RestDocumentationResultHandler signInApiDocument() {
-        return document("user/signin", ApiDocumentUtil.getDocumentRequest(),
+        return document("user/signin",
+                        ApiDocumentUtil.getDocumentRequest(),
                         requestHeaders(
-                        headerWithName(HttpHeaders.CONTENT_TYPE).description(MediaType.APPLICATION_JSON)
-                    ),
+                            headerWithName(HttpHeaders.CONTENT_TYPE).description(MediaType.APPLICATION_JSON)
+                        ),
                         requestFields(
-                        fieldWithPath("userId").description("로그인 - 유저ID"),
-                        fieldWithPath("password").description("로그인 - 비밀번호")
-                    ),
+                            fieldWithPath("userId").description("로그인 - 유저ID"),
+                            fieldWithPath("password").description("로그인 - 비밀번호")
+                        ),
                         responseFields(
                             ApiDocumentUtil.makeResponseFields(
-                            fieldWithPath("data.token").description("인증된 사용자의 Token 값"),
-                            fieldWithPath("data.links").optional().type(JsonFieldType.ARRAY).description("인증토큰 발급 api profile 링크"),
-                            fieldWithPath("data.links[].*").ignored()
+                                fieldWithPath("data.token").description("인증된 사용자의 Token 값"),
+                                fieldWithPath("data.links").optional().type(JsonFieldType.ARRAY).description("인증토큰 발급 api profile 링크"),
+                                fieldWithPath("data.links[].*").ignored()
+                            )
                         )
-                    )
-                );
+        );
     }
 
 }
